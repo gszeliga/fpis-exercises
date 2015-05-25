4e613468934f7445e3cb699007400042f2578a24
guillermo <gszeliga@gmail.com>
2015-05-26 00:58:01 +0200

    Trying to see how 'pipe' operation actually works (brain melting exercise)

--- src/main/scala/cn/fpis/exercises/ch15/Process.scala
+++ src/main/scala/cn/fpis/exercises/ch15/Process.scala
@@ -10,44 +10,55 @@ trait Process[I,O]{
   def apply(s: Stream[I]):Stream[O] = this match {
     case Halt() => Stream()
     case Await(recv,finalizer) => s match {
-      case h #:: t => recv(h)(t) //We keep the interpretation loop by passing tail to 'apply'
+      case h #:: t => recv(Some(h))(t) //We keep the interpretation loop by passing tail to 'apply'
       case _ => finalizer(s)
     }
-    case Emit(h,t) => h.toStream append t(s)
+    case Emit(h,t) => Stream(h) append t(s)
   }
 
   def map[O2](f: O => O2): Process[I,O2] = this match {
     case Halt() => Halt()
     case Await(recv, finalizer) => Await(recv andThen (_ map f), finalizer map f)
-    case Emit(h,t) =>  Emit(h map f, t map f)
+    case Emit(h,t) =>  Emit(f(h), t map f)
   }
 
   def ++(p: => Process[I,O]): Process[I,O] = this match {
 
     case Halt() => p
     case Await(recv, finalizer) => Await(i => recv(i) ++ p, finalizer ++ p)
-    case Emit(h,t) => emitAll(h,t ++ p)
+    case Emit(h,t) => Emit(h,t ++ p)
   }
 
-  def emitAll[I,O](head: Seq[O], tail: Process[I,O] = Halt[I,O]()): Process[I,O] = {
+/*  def emitAll[I,O](head: Seq[O], tail: Process[I,O] = Halt[I,O]()): Process[I,O] = {
     tail match {
       case Emit(h,t) => Emit(head ++ h, t)
       case _ => Emit(head, tail)
     }
-  }
+  }*/
 
-  def emit[I,O](head: O, tail: Process[I,O] = Halt[I,O]):Process[I,O] = emitAll(Stream(head),tail)
+  def emit[I,O](head: O, tail: Process[I,O] = Halt[I,O]):Process[I,O] = Emit(head,tail)
 
   def flatMap[O2](f: O => Process[I,O2]): Process[I,O2] = this match {
     case Halt() => Halt()
-    case Emit(h,t) => {
-      if (h.isEmpty) t flatMap f
-      else f(h.head) ++ emitAll(h.tail,t).flatMap(f)
-    }
+    case Emit(h,t) => f(h) ++ t.flatMap(f)
     case Await(recv, finalizer) => Await(i => recv(i) flatMap f, finalizer flatMap f)
   }
 
   def unit[I,O](v:O): Process[I,O] = emit(v)
+
+  //'pipe' or 'compose'
+  def |>[O2](p2: Process[O,O2]): Process[I,O2] = {
+
+    p2 match {
+      case Halt() => Halt()
+      case Emit(h,t) => Emit(h,this |> t)
+      case Await(f,finalizer) => this match {
+        case Emit(h,t) => t |> f(Some(h))
+        case Halt() => Halt() |> f(None)
+        case Await(g,finalizer2) =>  Await(i => g(i) |> p2)
+      }
+    }
+  }
 
 }
 
@@ -55,7 +66,7 @@ object Process{
 
   //Partially apply the I parameter of Process
   def monad[I] = new Monad[({ type f[x] = Process[I,x]})#f]{
-    override def unit[O](v: => O): Process[I, O] = Emit(Stream(v))
+    override def unit[O](v: => O): Process[I, O] = Emit(v)
     override def flatMap[O, O2](ma: Process[I, O])(f: (O) => Process[I, O2]): Process[I, O2] = ma flatMap f
   }
 
@@ -71,11 +82,11 @@ case class Halt[I,O]() extends Process[I,O]
 
 /*Indicates to the driver that the head values should be emitted to the output stream,
  and that tail should be the next state following that*/
-case class Emit[I,O](head: Seq[O], tail: Process[I,O] = Halt[I,O]) extends Process[I,O]
+case class Emit[I,O](head: O, tail: Process[I,O] = Halt[I,O]) extends Process[I,O]
 
 /*
 * Requests a value from the input stream, indicating that 'recv'
 should be used by the driver to produce the next state, and that finalizer should be
 consulted if the input has no more elements available.
 * */
-case class Await[I,O](recv: I => Process[I,O], finalizer: Process[I,O] = Halt[I,O]) extends Process[I,O]
+case class Await[I,O](recv: Option[I] => Process[I,O], finalizer: Process[I,O] = Halt[I,O]) extends Process[I,O]
